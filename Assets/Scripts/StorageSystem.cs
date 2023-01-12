4fab30e9e056fef15fec29d43ae4685146da9878
Vylaxez <vylaxez@gmail.com>
2023-01-12 10:22:28 +0100

    documented StorageSystem and started implementing Action class

--- Assets/Scripts/StorageSystem.cs
+++ Assets/Scripts/StorageSystem.cs
@@ -23,6 +23,12 @@ public class StorageSystem : MonoBehaviour
             }
         }
 
+        /// <summary>
+        /// Storage class used to implement item storage (players' inventories, players' stashes, item containers)
+        /// </summary>
+        /// <param name="width">number of slots columns in the storage</param>
+        /// <param name="height">number of slots rows in the storage</param>
+        /// <param name="lockedSlots">boolean matrix containing the value true at the coordinates of each locked slots</param>
         public Storage(int width, int height, bool[,] lockedSlots = null)
         {
             this.width = width;
@@ -37,11 +43,22 @@ public class StorageSystem : MonoBehaviour
             }
         }
 
+        /// <summary>
+        /// Check if the slot position is within storage bounds and isn't locked
+        /// </summary>
         public bool SlotInBounds(int x, int y)
         {
             return IsWithin(0, x, this.width - 1) && IsWithin(0, y, this.height - 1) && !slots[x, y].locked;
         }
 
+        /// <summary>
+        /// Get the slots in the defined area
+        /// </summary>
+        /// <param name="x">x position of the area top left slot</param>
+        /// <param name="y">y position of the area top left slot</param>
+        /// <param name="areaWidth">number of columns of the area</param>
+        /// <param name="areaHeight">number of rows of the area</param>
+        /// <returns>An matrix containing the area slots</returns>
         public Slot[,] AreaSlots(int x, int y, int areaWidth, int areaHeight)
         {
             Slot[,] area = new Slot[areaWidth, areaHeight];
@@ -55,6 +72,10 @@ public class StorageSystem : MonoBehaviour
             return area;
         }
 
+        /// <summary>
+        /// Check if all slots in the area are free (not locked and not full)
+        /// </summary>
+        /// <param name="area">The slots area matrix</param>
         public bool AreaFree(Slot[,] area)
         {
             if(area.GetLength(0) < 1 || area.GetLength(1) < 1)
@@ -75,6 +96,13 @@ public class StorageSystem : MonoBehaviour
             return true;
         }
 
+        /// <summary>
+        /// Place an item to the given position
+        /// </summary>
+        /// <param name="x">The x position where the top left slot of the item should be placed</param>
+        /// <param name="y">The y position where the top left slot of the item should be placed</param>
+        /// <param name="item">The item to place</param>
+        /// <returns>True if the item was successfully placed</returns>
         public bool PlaceItem(int x, int y, Item item)
         {
             int itemWidth = item.Width;
@@ -97,6 +125,12 @@ public class StorageSystem : MonoBehaviour
             return true;
         }
 
+        /// <summary>
+        /// Remove and item from the storage
+        /// </summary>
+        /// <param name="x">x coordinate of a slot containing the item to remove</param>
+        /// <param name="y">y coordinate of a slot containing the item to remove</param>
+        /// <returns>True if the item was successfully removed from the storage</returns>
         public bool RemoveItem(int x, int y)
         {
             Item item = slots[x, y].item;
@@ -126,7 +160,16 @@ public class StorageSystem : MonoBehaviour
             return true;
         }
 
-        public bool MoveItem(int x, int y, int newX, int newY)
+        /// <summary>
+        /// Move item to the given position in storage, if no storage is given the item is placed in the same storage it is moved from
+        /// </summary>
+        /// <param name="x">The x slot position of a slot containing the item to move</param>
+        /// <param name="y">The y slot position of a slot containing the item to move</param>
+        /// <param name="newX">The desired x top left slot position for the item in the destination storage</param>
+        /// <param name="newY">The desired y top left slot position for the item in the destination storage</param>
+        /// <param name="storage">The destination storage for the item, default is the item's original storage</param>
+        /// <returns>True if the item was moved successfully</returns>
+        public bool MoveItem(int x, int y, int newX, int newY, Storage storage=this)
         {
             Item item = slots[x, y].item;
             
@@ -135,7 +178,7 @@ public class StorageSystem : MonoBehaviour
                 return false;
             }
 
-            if (!PlaceItem(newX, newY, item))
+            if (!storage.PlaceItem(newX, newY, item))
             {
                 PlaceItem(x, y, item);
                 return false;
@@ -143,6 +186,12 @@ public class StorageSystem : MonoBehaviour
             return true;
         }
 
+        /// <summary>
+        /// Switch items position in the current storage
+        /// </summary>
+        /// <param name="item1">First item</param>
+        /// <param name="item2">Second item</param>
+        /// <returns>True if the item was moved successfully</returns>
         public bool SwitchItems(Item item1, Item item2)
         {
             if(item1.parents == null || item2.parents == null || item1.parents.GetLength(0)*item1.parents.GetLength(1)<1 || item2.parents.GetLength(0) * item2.parents.GetLength(1) < 1)
@@ -201,6 +250,9 @@ public class StorageSystem : MonoBehaviour
             this.locked = locked;
         }
 
+        /// <summary>
+        /// Returns true when the slot is free (not locked and not full)
+        /// </summary>
         public bool IsFree => !(locked || full);
 
         public void PlaceItem(Item item)
@@ -231,7 +283,13 @@ public class StorageSystem : MonoBehaviour
             this.parents = parents;
         }
 
+        /// <summary>
+        /// returns the relative width of the item depending on its rotation
+        /// </summary>
         public int Width => rotated ? itemsData[id].width : itemsData[id].height;
+        /// <summary>
+        /// returns the relative height of the item depending on its rotation
+        /// </summary>
         public int Height => rotated ? itemsData[id].height : itemsData[id].width;
 
         public void Place(Slot[,] area)
@@ -242,6 +300,36 @@ public class StorageSystem : MonoBehaviour
         public void Remove()
         {
             parents = null;
+        }
+    }
+
+    public class Action
+    {
+        public int id;
+        public Storage oldStorage;
+        public Storage destStorage;
+        public Item item;
+        private int code = -1;
+
+        public Action(int id, Storage oldStorage, Storage destStorage, Item item)
+        {
+            this.id = id;
+            this.oldStorage = oldStorage;
+            this.destStorage = destStorage;
+        }
+
+
+        public int ActionCode
+        {
+            get
+            {
+                if (code == -1)
+                {
+                    //compute code
+
+                }
+                return code;
+            }
         }
     }
 }
